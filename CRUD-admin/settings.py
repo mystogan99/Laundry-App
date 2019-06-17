c5b02697cc13db874ac2c09f92820bcf0eb24ae1
hattricks98 <hrithiky328@gmail.com>
2019-06-18 03:09:26 +0530

    added owl carsoul

--- CRUD-admin/settings.py
+++ CRUD-admin/settings.py
@@ -16,7 +16,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = (os.environ.get('DEBUG_VALUE')=='True')
 
-ALLOWED_HOSTS = ['laundrycity.herokuapp.com']
+ALLOWED_HOSTS = ['laundrycity.herokuapp.com', 'localhost', 'http://192.168.43.114']
 
 
 # Application definition
