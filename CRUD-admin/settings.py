e0f383cef10da68a527251c3fef7bb14bd946a7f
hattricks98 <hrithiky328@gmail.com>
2019-06-18 00:11:56 +0530

    Added static path

--- CRUD-admin/settings.py
+++ CRUD-admin/settings.py
@@ -108,7 +108,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = '/HolyGrail'
